19a4c739edc72139df15efba228d4c21686b8f65
Charles Neyrand <charles.neyrand@outlook.fr>
2024-03-01 11:41:33 +0100

    Add tab and S-tab for vsnip snippet

--- lua/carolus/config/keymaps.lua
+++ lua/carolus/config/keymaps.lua
@@ -49,3 +49,9 @@ keymap.set("n", "ii", "<Esc>")
 keymap.set("i", "ij", "<Esc>")
 
 
+
+-- Jump forward or backward
+vim.api.nvim_set_keymap('i', '<Tab>', 'vsnip#jumpable(1) ? "<Plug>(vsnip-jump-next)" : "<Tab>"', {expr = true, noremap = true})
+vim.api.nvim_set_keymap('s', '<Tab>', 'vsnip#jumpable(1) ? "<Plug>(vsnip-jump-next)" : "<Tab>"', {expr = true, noremap = true})
+vim.api.nvim_set_keymap('i', '<S-Tab>', 'vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<S-Tab>"', {expr = true, noremap = true})
+vim.api.nvim_set_keymap('s', '<S-Tab>', 'vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<S-Tab>"', {expr = true, noremap = true})
